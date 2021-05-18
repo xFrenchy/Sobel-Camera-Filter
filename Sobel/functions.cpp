73585fd539042b5a79f68429ef9259b1db41c392
Anthony <Frenchy2199@yahoo.com>
2021-05-18 17:15:01 -0600

    Added more comments for readability

--- Sobel/functions.cpp
+++ Sobel/functions.cpp
@@ -6,6 +6,7 @@ Mat rainbowSobel(Mat frame) {
 	Mat grad, grad_x, grad_y, abs_grad_x, abs_grad_y;
 	int ddepth = CV_16S;
 
+	//Sobel(input array without grayscale, output array, depth of output image, dx, dy, ksize, scale, delta, bordertype)
 	Sobel(frame, grad_x, ddepth, 1, 0, 1, 1, 0, BORDER_DEFAULT);
 	Sobel(frame, grad_y, ddepth, 0, 1, 1, 1, 0, BORDER_DEFAULT);
 
@@ -27,6 +28,7 @@ Mat ProperSobel(Mat frame) {
 	// Convert the image to grayscale
 	cvtColor(src, src_gray, COLOR_BGR2GRAY);
 
+	//Sobel(input array in grayscale, output array, depth of output image, dx, dy, ksize, scale, delta, bordertype)
 	Sobel(src_gray, grad_x, ddepth, 1, 0, 1, 1, 0, BORDER_DEFAULT);
 	Sobel(src_gray, grad_y, ddepth, 0, 1, 1, 1, 0, BORDER_DEFAULT);
 
@@ -39,11 +41,18 @@ Mat ProperSobel(Mat frame) {
 }
 
 Mat colorSobel(Mat frame, EFilter_Sobel color) {
+	//Apply sobel to current frame
 	frame = ProperSobel(frame);
+
+	//Create a mask of all the pixel that should represent the sobel outline
 	Mat mask;
 	inRange(frame, Scalar(10, 10, 10), Scalar(255, 255, 255), mask);
+
+	//Turn it back into RGB
 	Mat Cimage;
 	cvtColor(frame, Cimage, COLOR_GRAY2RGB);
+
+	//Change sobel outline from white to a specific color
 	int maskColor[3];
 	switch (color) {
 		case EFilter_Sobel::green:
@@ -88,6 +97,7 @@ Mat colorSobel(Mat frame, EFilter_Sobel color) {
 		std::cout << "End of column\n\n";
 	}*/
 
+	//Apply the new colors to the mask of sobel outlines and update the current image
 	Cimage.setTo(Scalar(maskColor[2], maskColor[1], maskColor[0]), mask);	//opencb uses BGR
 	return Cimage;
 }
@@ -96,32 +106,32 @@ Mat applySobel(Mat frame, EFilter_Sobel status) {
 	Mat image;
 	static int colorRotation = 0;
 	switch (status) {
-	case EFilter_Sobel::rainbow:
-		image = rainbowSobel(frame);
-		break;
-	case EFilter_Sobel::proper:
-		image = ProperSobel(frame);
-		break;
-	case EFilter_Sobel::green:
-		image = colorSobel(frame, EFilter_Sobel::green);
-		break;
-	case EFilter_Sobel::red:
-		image = colorSobel(frame, EFilter_Sobel::red);
-		break;
-	case EFilter_Sobel::gold:
-		image = colorSobel(frame, EFilter_Sobel::gold);
-		break;
-	case EFilter_Sobel::purple:
-		image = colorSobel(frame, EFilter_Sobel::purple);
-		break;
-	case EFilter_Sobel::rotate_all_colors: {
-		EFilter_Sobel colors[] = { EFilter_Sobel::green, EFilter_Sobel::red, EFilter_Sobel::gold, EFilter_Sobel::purple };	//needs manual update, not good
-		image = colorSobel(frame, colors[colorRotation]);
-		colorRotation = ++colorRotation % (sizeof(colors) / sizeof(colors[0]));
-		break;
-	}
-	default:
-		std::cout << "### CURRENT STATUS NOT SUPPORTED ###\n";
+		case EFilter_Sobel::rainbow:
+			image = rainbowSobel(frame);
+			break;
+		case EFilter_Sobel::proper:
+			image = ProperSobel(frame);
+			break;
+		case EFilter_Sobel::green:
+			image = colorSobel(frame, EFilter_Sobel::green);
+			break;
+		case EFilter_Sobel::red:
+			image = colorSobel(frame, EFilter_Sobel::red);
+			break;
+		case EFilter_Sobel::gold:
+			image = colorSobel(frame, EFilter_Sobel::gold);
+			break;
+		case EFilter_Sobel::purple:
+			image = colorSobel(frame, EFilter_Sobel::purple);
+			break;
+		case EFilter_Sobel::rotate_all_colors: {
+			EFilter_Sobel colors[] = { EFilter_Sobel::green, EFilter_Sobel::red, EFilter_Sobel::gold, EFilter_Sobel::purple };	//needs manual update, not good
+			image = colorSobel(frame, colors[colorRotation]);
+			colorRotation = ++colorRotation % (sizeof(colors) / sizeof(colors[0]));
+			break;
+		}
+		default:
+			std::cout << "### CURRENT STATUS NOT SUPPORTED ###\n";
 	}
 
 	return image;
@@ -133,45 +143,45 @@ EFilter_Sobel userInput()
 	int keyInput = waitKey(1);
 	EFilter_Sobel status;
 	switch (keyInput) {
-	case ESC_KEY:
-		std::cout << "Esc key is pressed by user. Stoppig the video\n";
-		exit(0);
-		break;
-	case ONE_KEY:
-		std::cout << "Switching to rainbow sobel\n";
-		status = EFilter_Sobel::rainbow;
-		break;
-	case TWO_KEY:
-		std::cout << "Switching to proper sobel\n";
-		status = EFilter_Sobel::proper;
-		break;
-	case THREE_KEY:
-		std::cout << "Switching to green sobel\n";
-		status = EFilter_Sobel::green;
-		break;
-	case FOUR_KEY:
-		std::cout << "Switching to red sobel\n";
-		status = EFilter_Sobel::red;
-		break;
-	case FIVE_KEY:
-		std::cout << "Switching to gold sobel\n";
-		status = EFilter_Sobel::gold;
-		break;
-	case SIX_KEY:
-		std::cout << "Switching to purple sobel\n";
-		status = EFilter_Sobel::purple;
-		break;
-	case SEVEN_KEY:
-		std::cout << "Switching to all color rotation sobel\n";
-		status = EFilter_Sobel::rotate_all_colors;
-		break;
-	case -1:
-		status = EFilter_Sobel::no_change;
-		break;
-	default:
-		status = EFilter_Sobel::no_change;
-		std::cout << char(keyInput) << " is not a valid input\n";
-		break;
+		case ESC_KEY:
+			std::cout << "Esc key is pressed by user. Stoppig the video\n";
+			exit(0);
+			break;
+		case ONE_KEY:
+			std::cout << "Switching to rainbow sobel\n";
+			status = EFilter_Sobel::rainbow;
+			break;
+		case TWO_KEY:
+			std::cout << "Switching to proper sobel\n";
+			status = EFilter_Sobel::proper;
+			break;
+		case THREE_KEY:
+			std::cout << "Switching to green sobel\n";
+			status = EFilter_Sobel::green;
+			break;
+		case FOUR_KEY:
+			std::cout << "Switching to red sobel\n";
+			status = EFilter_Sobel::red;
+			break;
+		case FIVE_KEY:
+			std::cout << "Switching to gold sobel\n";
+			status = EFilter_Sobel::gold;
+			break;
+		case SIX_KEY:
+			std::cout << "Switching to purple sobel\n";
+			status = EFilter_Sobel::purple;
+			break;
+		case SEVEN_KEY:
+			std::cout << "Switching to all color rotation sobel\n";
+			status = EFilter_Sobel::rotate_all_colors;
+			break;
+		case -1:
+			status = EFilter_Sobel::no_change;
+			break;
+		default:
+			status = EFilter_Sobel::no_change;
+			std::cout << char(keyInput) << " is not a valid input\n";
+			break;
 	}
 	
 	return status;
