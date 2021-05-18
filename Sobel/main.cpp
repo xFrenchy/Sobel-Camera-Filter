eed14268fdbc1d1f630da5060aa4ce702aeb3a19
Anthony <Frenchy2199@yahoo.com>
2021-05-18 17:00:32 -0600

    Unclutured the main function

--- Sobel/main.cpp
+++ Sobel/main.cpp
@@ -11,17 +11,6 @@
 
 #include "functions.h"
 
-#define ESC_KEY 27
-#define ONE_KEY 49
-#define TWO_KEY 50
-#define THREE_KEY 51
-#define FOUR_KEY 52
-#define FIVE_KEY 53
-#define SIX_KEY 54
-#define SEVEN_KEY 55
-#define EIGHT_KEY 56
-#define NINE_KEY 57
-
 using namespace cv;
 using namespace std;
 
@@ -42,48 +31,10 @@ int main(int argc, char** argv)
 	namedWindow(windowName); // Create a window
 	EFilter_Sobel status = EFilter_Sobel::rainbow;
 
-	int colorRotation = 0;
 	while (true) {
-		//I want this switching to be better
-		int keyInput = waitKey(1);
-		switch (keyInput) {
-			case ESC_KEY:
-				cout << "Esc key is pressed by user. Stoppig the video" << endl;
-				exit(0);
-				break;
-			case ONE_KEY:
-				cout << "Switching to rainbow sobel\n";
-				status = EFilter_Sobel::rainbow;
-				break;
-			case TWO_KEY:
-				cout << "Switching to proper sobel\n";
-				status = EFilter_Sobel::proper;
-				break;
-			case THREE_KEY:
-				cout << "Switching to green sobel\n";
-				status = EFilter_Sobel::green;
-				break;
-			case FOUR_KEY:
-				cout << "Switching to red sobel\n";
-				status = EFilter_Sobel::red;
-				break;
-			case FIVE_KEY:
-				cout << "Switching to gold sobel\n";
-				status = EFilter_Sobel::gold;
-				break;
-			case SIX_KEY:
-				cout << "Switching to purple sobel\n";
-				status = EFilter_Sobel::purple;
-				break;
-			case SEVEN_KEY:
-				cout << "Switching to all color rotation sobel\n";
-				status = EFilter_Sobel::rotate_all_colors;
-				break;
-			case -1:
-				break;
-			default:
-				cout << char(keyInput) << " is not a valid input\n";
-				break;
+		EFilter_Sobel newStatus = userInput();
+		if (newStatus != EFilter_Sobel::no_change) {
+			status = newStatus;
 		}
 
 		Mat frame;
@@ -97,35 +48,7 @@ int main(int argc, char** argv)
 			break;
 		}
 
-		Mat image;
-		switch (status){
-			case EFilter_Sobel::rainbow:
-				image = rainbowSobel(frame);
-				break;
-			case EFilter_Sobel::proper:
-				image = ProperSobel(frame);
-				break;
-			case EFilter_Sobel::green:
-				image = colorSobel(frame, EFilter_Sobel::green);
-				break;
-			case EFilter_Sobel::red:
-				image = colorSobel(frame, EFilter_Sobel::red);
-				break;
-			case EFilter_Sobel::gold:
-				image = colorSobel(frame, EFilter_Sobel::gold);
-				break;
-			case EFilter_Sobel::purple:
-				image = colorSobel(frame, EFilter_Sobel::purple);
-				break;
-			case EFilter_Sobel::rotate_all_colors: {
-				EFilter_Sobel colors[] = { EFilter_Sobel::green, EFilter_Sobel::red, EFilter_Sobel::gold, EFilter_Sobel::purple };	//needs manual update, not good
-				image = colorSobel(frame, colors[colorRotation]);
-				colorRotation = ++colorRotation % (sizeof(colors) / sizeof(colors[0]));
-				break;
-			}
-			default:
-				cout << "### CURRENT STATUS NOT SUPPORTED ###\n";
-		}
+		Mat image = applySobel(frame, status);
 		
 		imshow(windowName, image);
 
