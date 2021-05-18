334330150e98803b6076b89a7bb6d76a13d97efb
Anthony <Frenchy2199@yahoo.com>
2021-05-18 17:01:00 -0600

    Added more Sobel logic to the file and moved the user input in here as well

--- Sobel/functions.cpp
+++ Sobel/functions.cpp
@@ -91,3 +91,88 @@ Mat colorSobel(Mat frame, EFilter_Sobel color) {
 	Cimage.setTo(Scalar(maskColor[2], maskColor[1], maskColor[0]), mask);	//opencb uses BGR
 	return Cimage;
 }
+
+Mat applySobel(Mat frame, EFilter_Sobel status) {
+	Mat image;
+	static int colorRotation = 0;
+	switch (status) {
+	case EFilter_Sobel::rainbow:
+		image = rainbowSobel(frame);
+		break;
+	case EFilter_Sobel::proper:
+		image = ProperSobel(frame);
+		break;
+	case EFilter_Sobel::green:
+		image = colorSobel(frame, EFilter_Sobel::green);
+		break;
+	case EFilter_Sobel::red:
+		image = colorSobel(frame, EFilter_Sobel::red);
+		break;
+	case EFilter_Sobel::gold:
+		image = colorSobel(frame, EFilter_Sobel::gold);
+		break;
+	case EFilter_Sobel::purple:
+		image = colorSobel(frame, EFilter_Sobel::purple);
+		break;
+	case EFilter_Sobel::rotate_all_colors: {
+		EFilter_Sobel colors[] = { EFilter_Sobel::green, EFilter_Sobel::red, EFilter_Sobel::gold, EFilter_Sobel::purple };	//needs manual update, not good
+		image = colorSobel(frame, colors[colorRotation]);
+		colorRotation = ++colorRotation % (sizeof(colors) / sizeof(colors[0]));
+		break;
+	}
+	default:
+		std::cout << "### CURRENT STATUS NOT SUPPORTED ###\n";
+	}
+
+	return image;
+}
+
+EFilter_Sobel userInput()
+{
+	//I want this switching to be better
+	int keyInput = waitKey(1);
+	EFilter_Sobel status;
+	switch (keyInput) {
+	case ESC_KEY:
+		std::cout << "Esc key is pressed by user. Stoppig the video\n";
+		exit(0);
+		break;
+	case ONE_KEY:
+		std::cout << "Switching to rainbow sobel\n";
+		status = EFilter_Sobel::rainbow;
+		break;
+	case TWO_KEY:
+		std::cout << "Switching to proper sobel\n";
+		status = EFilter_Sobel::proper;
+		break;
+	case THREE_KEY:
+		std::cout << "Switching to green sobel\n";
+		status = EFilter_Sobel::green;
+		break;
+	case FOUR_KEY:
+		std::cout << "Switching to red sobel\n";
+		status = EFilter_Sobel::red;
+		break;
+	case FIVE_KEY:
+		std::cout << "Switching to gold sobel\n";
+		status = EFilter_Sobel::gold;
+		break;
+	case SIX_KEY:
+		std::cout << "Switching to purple sobel\n";
+		status = EFilter_Sobel::purple;
+		break;
+	case SEVEN_KEY:
+		std::cout << "Switching to all color rotation sobel\n";
+		status = EFilter_Sobel::rotate_all_colors;
+		break;
+	case -1:
+		status = EFilter_Sobel::no_change;
+		break;
+	default:
+		status = EFilter_Sobel::no_change;
+		std::cout << char(keyInput) << " is not a valid input\n";
+		break;
+	}
+	
+	return status;
+}
--- Sobel/functions.h
+++ Sobel/functions.h
@@ -2,6 +2,17 @@
 
 #include <opencv2/opencv.hpp>
 
+#define ESC_KEY 27
+#define ONE_KEY 49
+#define TWO_KEY 50
+#define THREE_KEY 51
+#define FOUR_KEY 52
+#define FIVE_KEY 53
+#define SIX_KEY 54
+#define SEVEN_KEY 55
+#define EIGHT_KEY 56
+#define NINE_KEY 57
+
 enum class EFilter_Sobel
 {
 	rainbow,
@@ -11,6 +22,7 @@ enum class EFilter_Sobel
 	gold,
 	purple,
 	rotate_all_colors,
+	no_change,
 };
 
 using namespace cv;
@@ -18,3 +30,6 @@ using namespace cv;
 Mat rainbowSobel(Mat);
 Mat ProperSobel(Mat);
 Mat colorSobel(Mat, EFilter_Sobel);
+Mat applySobel(Mat, EFilter_Sobel);
+
+EFilter_Sobel userInput();
